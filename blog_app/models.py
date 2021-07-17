000eec9c147aa2ca82888807267f9e37baf4449e
Tucker Barton <hisashiburix@gmail.com>
2021-07-17 12:29:33 -0400

    Fix bug where slug field wasnt being populated automatically when form was submitted.

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.fields.related import ManyToManyField
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
@@ -20,3 +21,7 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
