d373a978cb69016489ef1b01ead6c544ef0914a1
Tucker Barton <hisashiburix@gmail.com>
2021-07-19 19:47:58 -0400

    Add svgs to pages. Update footer & add-post pages.

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -8,5 +8,11 @@ class PostForm(forms.Form):
     image_name = forms.CharField(label="イメージ名",max_length=100)
     tags = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple, queryset = Tag.objects.all())
 
+    def clean_tags(self):
+        value = self.cleaned_data['tags']
+        if len(value) > 4:
+            raise forms.ValidationError("You can't select more than 4 tags per post.")
+        return value
+
 class TagForm(forms.Form):
     caption = forms.CharField(label="キャプション", max_length=20)
