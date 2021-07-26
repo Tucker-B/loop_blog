d47abf3d2843600b1f7273266c3519bf31d6c7e3
Tucker Barton <hisashiburix@gmail.com>
2021-07-25 22:14:19 -0400

    Add ProcFile, other things for Heroku Deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn loop_blog.wsgi
--- loop_blog/settings.py
+++ loop_blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from os import getenv
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+django_on_heroku.settings(locals())
