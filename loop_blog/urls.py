4e3ffd64275ee6b6ec3a519cebc812ee0dcb79b0
Tucker Barton <hisashiburix@gmail.com>
2021-07-25 22:39:08 -0400

    Fix static folder error when debug is false.

--- loop_blog/urls.py
+++ loop_blog/urls.py
@@ -14,12 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_app.urls')),
     path('accounts/', include('django.contrib.auth.urls'), name="login"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
+] 
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
